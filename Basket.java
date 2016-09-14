75df6e3989f6487f53198b06817dbc080f04069b
Alan Russell <ajrussellaudio@gmail.com>
2016-09-14 16:24:19 +0100

    implement basket

--- Basket.java
+++ Basket.java
@@ -2,4 +2,28 @@ import java.util.*;
 
 class Basket {
 
+  ArrayList<ShoppingItem> items;
+
+  public Basket() {
+    this.items = new ArrayList<ShoppingItem>();
+  }
+
+  public int itemsCount() {
+    return items.size();
+  }
+
+  public void add(ShoppingItem item) {
+    items.add(item);
+  }
+
+  public void remove(ShoppingItem item) {
+    items.remove(item);
+  }
+
+  public void empty() {
+    for( int i = 0; i <= itemsCount(); i++ ) {
+      items.remove(0);
+    }
+  }
+
 }
