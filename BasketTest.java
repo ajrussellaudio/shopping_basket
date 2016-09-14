41240bb80239940902d0af0a933f6b617c700308
Alan Russell <ajrussellaudio@gmail.com>
2016-09-14 16:15:33 +0100

    add basket test

--- BasketTest.java
+++ BasketTest.java
@@ -3,6 +3,36 @@ import org.junit.*;
 
 public class BasketTest {
 
+  ShoppingItem bread = new ShoppingItem(0.89, "Hovis white loaf");
+  ShoppingItem milk = new ShoppingItem(0.99, "Whole milk 2L");
+  ShoppingItem eggs = new ShoppingItem(1.25, "Free range eggs x 6");
   Basket basket;
+
+  @Before
+  public void before() {
+    basket = new Basket();
+  }
+
+  @Test
+  public void canAddItems() {
+    basket.add(bread);
+    assertEquals(1, basket.itemsCount());
+  }
+
+  @Test
+  public void canRemoveItems() {
+    basket.add(milk);
+    basket.add(bread);
+    basket.remove(milk);
+    assertEquals(1, basket.itemsCount());
+  }
+
+  @Test
+  public void canEmptyBasket() {
+    basket.add(milk);
+    basket.add(bread);
+    basket.empty();
+    assertEquals(0, basket.itemsCount());
+  }
 
 }
